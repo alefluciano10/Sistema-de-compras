ec9876eae0b7d51e9a5d86d4c92d9eadb377545f
Alef Luciano <alef.luciano@gmail.com>
2025-10-02 12:55:32 -0300

    Classe cliente

--- src/Sistema/modelo/Cliente.java
+++ src/Sistema/modelo/Cliente.java
@@ -1,4 +1,69 @@
 package Sistema.modelo;
 
+import java.util.List;
+
+/* Criação da classe Cliente */
+
 public class Cliente {
+    private String nome;
+    private int cpf;
+    private String senha;
+    private String email;
+    private int telefone;
+    private String endereco;
+    private CartaoDeCredito cartao;
+    private List<Compra> compras;
+
+    /* Construtor da classe Cliente */
+
+    public Cliente(String nome, int cpf, String senha, String email, int telefone, String endereco, CartaoDeCredito cartao, List<Compra> compras) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.cartao = cartao;
+        this.compras = compras;
+    }
+
+    /* Getters */
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public CartaoDeCredito getCartao() {
+        return cartao;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+
+    /* Metódo para adicionar a compra do cliente */
+
+    public void adicionarCompra(Compra compra) {
+        compras.add(compra);
+    }
 }
