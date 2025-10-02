1280a0aae28ed7c1431122244d7491fde560cbab
Alef Luciano <alef.luciano@gmail.com>
2025-10-02 13:47:35 -0300

    Classe compra

--- src/Sistema/modelo/Compra.java
+++ src/Sistema/modelo/Compra.java
@@ -1,4 +1,38 @@
 package Sistema.modelo;
 
+/* Criação da classe Compra */
+
 public class Compra {
+    private Produto produto;
+    private int quantidade;
+    private int valorTotal;
+
+    /* Construtor */
+
+    public Compra(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.valorTotal = produto.getPreco() * quantidade;
+    }
+
+    /* Getters */
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public int getValorTotal() {
+        return valorTotal;
+    }
+
+    /* Sobrescrever */
+
+    @Override
+    public String toString() {
+        return produto.getNome() + " | Qtd: " + quantidade + " | Total: R$ " + valorTotal;
+    }
 }
