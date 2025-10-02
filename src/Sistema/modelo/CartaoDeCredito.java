686c4991902cd3adbd27c127f28301d4312f5798
Alef Luciano <alef.luciano@gmail.com>
2025-10-02 13:13:51 -0300

    Classe cartão de crédito

--- src/Sistema/modelo/CartaoDeCredito.java
+++ src/Sistema/modelo/CartaoDeCredito.java
@@ -1,4 +1,49 @@
 package Sistema.modelo;
 
+/* Criação da classe CartãoDeCredito */
+
 public class CartaoDeCredito {
+    private int numero;
+    private double limite;
+    private double saldo;
+
+    /* Construtor da classe */
+
+    public CartaoDeCredito(int numero, double limite, double saldo) {
+        this.numero = numero;
+        this.limite = limite;
+        this.saldo = limite;
+    }
+
+    /* Getter */
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /* Metódo para realizar a compra */
+
+    public boolean autorizarCompra(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            return true;
+        }  else {
+            return false;
+        }
+    }
+
+    /* Metódo para mostar o cartão */
+
+    public void exibirCartãoDeCredito() {
+        System.out.println("=== Cartão de Credito ===");
+        System.out.println("Cartão: " + numero + " | Limite: " + limite + " | Saldo: " + saldo);
+    }
 }
